f385506adba689e9220d4064211ae9afd3500d25
abdulrfarag <ar060290@gmail.com>
2023-02-01 08:25:21 -0600

    added Navbar classname

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+function () {
+    return(
+        <>
+        <div className="Navbar">
+            <nav className="Navbar-container">
+                
+            </nav>
+        </div>
+        </>
+    )
+}
